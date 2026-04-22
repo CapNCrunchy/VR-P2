432c522c25cc01b4174b7e8ae1e8cbaf3263f9d5
Rosalinda Nancy "Lincy" Phipps <149025663+lincyphipps@users.noreply.github.com>
2026-04-22 10:17:39 -0400

    Added global back navigation and timer persistence
    Implemented B key navigation using new Input System.
    Ensured TimerSystem persists across scenes.

---
+++ Assets/Scripts/BackKey.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:417cf34cb4ef6af02ffb965ecbda400886d50be455569cd9b84d6fd28c7995dc
+size 323
---
+++ Assets/Scripts/DontDestroy.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:34f4ee9a5f82fe603415ec1d897abea1071789b6fc9ce2dc77fe3b93ea760e66
+size 340
